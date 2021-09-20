a170bb031a090c86c99c3d95589e922434e0358c
Stephen <stevriss22@gmail.com>
2021-09-20 13:30:40 -0400

    commit tree 67263a2488d74614e8b82783bbece8eea01089ae

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+   
+  if(distance < 400){
+    let greeting = 'This one is on me!';
+    return greeting;
+  }else if(distance < 2500){
+    let greeting = 'I will gladly take your thirty bucks.';
+    return greeting;
+  }else if(distance > 2500){
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if(city === 'NYC'){
+    return 'Ok, sounds good.';
+  }else{
+    return 'No go.'
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+
+  let generous;  
+  let notAsGenerous;
+  let ifAnythingElse;
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.';
+      break;
+  }
 }
